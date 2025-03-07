93c420919d8eb83351552f773b8e8e507d8edd70
Din-Kopyshchyk <dinisk182007@gmail.com>
2025-03-07 17:07:22 -0300

    Add files via upload

---
+++ js/nextPage.js
@@ -0,0 +1,7 @@
+document.getElementById("b-n2").onclick = function () {
+  location.href = "www.nextPage2.com";
+};
+
+document.getElementById("b-n3").onclick = function () {
+  location.href = "www.nextPage3.com";
+};
